fe0a4226b470b7f10daba1db3966ef32ea5f82c0
syouzan420 <syouzanponkikki@yahoo.co.jp>
2025-12-17 18:25:11 +0900

    update

--- src/SDLDraw.hs
+++ src/SDLDraw.hs
@@ -1,13 +1,18 @@
 module SDLDraw (draw) where
 
-import SDL (V4(V4),V2(V2))
+import SDL (V4(V4),V2(V2),Point(P))
+import SDL.Video.Renderer (copy,Rectangle(..))
 import SDL.Primitive (fillCircle)
 
-import MyData (MyData(mdRenderer,mdDouble))
+import MyData (MyData(mdRenderer,mdGetSprite,mdDouble))
+import SpriteName (SpriteName(PlayerFrontLeft))
 
 draw :: MyData -> IO ()
 draw md = do 
   let renderer = mdRenderer md
+  let getSprite = mdGetSprite md
+  let playerImage = getSprite PlayerFrontLeft
   let i = mdDouble md
   fillCircle renderer (V2 60 (10+fromIntegral(floor i))) 10 (V4 102 178 255 255)
+  copy renderer playerImage Nothing (Just (Rectangle (P (V2 10 10)) (V2 16 16)))
   putStrLn ("Hello " ++ show i)
